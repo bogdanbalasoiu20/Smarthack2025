8e0b025447f0d225079ca56f35cfae8156a6d16e
andreiusq <mrandyadv@gmail.com>
2025-11-08 17:20:43 +0200

    Fix presentations URL routing - remove p/ prefix to restore /api/presentations/ endpoint

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('user/', views.get_user, name='get_user'),
     path('roles/', views.list_roles, name='list_roles'),
 
-    # Presentation endpoints
-    path('p/', include('api.presentation_urls')),
+    # Presentation endpoints (mounted at root level to match /api/presentations/)
+    path('', include('api.presentation_urls')),
 ]
