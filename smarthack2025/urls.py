907e294583fbfdc4eee87343765ae0969502340b
andreiusq <mrandyadv@gmail.com>
2025-11-08 11:30:35 +0200

    Added alive message on /

--- smarthack2025/urls.py
+++ smarthack2025/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
+def home(request):
+    return JsonResponse({"message": "Backend alive", "go_to": "/api/hello/"})
+
 urlpatterns = [
+    path('', home),  # adaugă asta
     path('api/', include('api.urls')),
 ]
